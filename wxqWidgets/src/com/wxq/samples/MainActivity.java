5e5604894b96e458e95317e99f31080536638671
qingyiquan <goodbupt@gmail.com>
2014-02-07 14:21:48 +0800

    调整布局

--- wxqWidgets/src/com/wxq/samples/MainActivity.java
+++ wxqWidgets/src/com/wxq/samples/MainActivity.java
@@ -1,49 +1,33 @@
 package com.wxq.samples;
 
 import com.wxq.widgets.R;
-import com.wxq.widgets.SwipeView;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.content.Context;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
-import android.widget.ListView;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
-	ListView listView;
+	Button swipeViewListBtn;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-		listView = (ListView) findViewById(R.id.list);
-		String [] data = new String []{
-			"item1","item2","item3","item4","item5","item6","item7","item8"
-			,"item9","item10","item11","item12","item13"
-		};
-		ListAdapter adapter = new SwipeListViewAdaper(this, R.layout.listview_item, R.id.frontView, data);
-		listView.setAdapter(adapter);
+		setContentView(R.layout.activity_main_list);
+		swipeViewListBtn = (Button) findViewById(R.id.btn_swipe_view_list);
+		swipeViewListBtn.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// 打开滑动列表
+				startActivity(new Intent(MainActivity.this, SwipeViewListActivity.class));
+			}
+		});
 	}
 	
-	class SwipeListViewAdaper extends ArrayAdapter<String>
-	{
-		public SwipeListViewAdaper(Context context, int resource,
-				int textViewResourceId, String[] objects) {
-			super(context, resource, textViewResourceId, objects);
-		}
-
-		@Override
-		public View getView(int position, View convertView, ViewGroup parent) {
-			// TODO 自动生成的方法存根
-			SwipeView reuseView = (SwipeView) super.getView(position, convertView, parent);
-			reuseView.closeFront();
-			return reuseView;
-		}
-	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
